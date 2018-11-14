382905cfa84a6e9aa61b1c5bc3eb513fdfa6e3a0
Emik <emik.qeqw@mail.ru>
2018-11-14 11:29:25 +0300

    first

---
+++ lab1/say.js
@@ -0,0 +1,119 @@
+
+/*var a; // Объявление переменной a;
+a='Ivan';
+var b = 'Ivanov';
+var c = a +" "+ b;
+var a = "Programming";
+alert(c);
+console.log(c);
+var admin , name;
+name = 'Андрей';
+admin = name;
+alert(admin);
+console.log(admin, name);
+var A;
+A = oxfa;
+console.log(A);
+/*var some_variable =false;
+console.log(some_variable);
+console.log(typeof some_variable);
+console.log(typeof(NaN));*/
+/*var some_number=77.1234
+console.log(some_number);
+console.log(some_number.toExponential());
+console.log(some_number.toExponential(4));
+console.log(some_number.toExponential(2));
+var some_number = 77.1234
+console.log(some_number)
+console.log(some_number.toString())
+console.log(some_number.toString(16))
+console.log(typeof some_number.toString())
+console.log((10).toString())
+var some_number=77.123456789
+var some_number = 12345.6789
+console.log(some_number.toFixed())
+console.log(some_number.toFixed(1))
+console.log(some_number.toFixed(6))
+console.log((1.23e+10).toFixed(2))
+console.log((1.23e-10).toFixed(2))
+console.log(typeof 77.123456);
+console.log(typeof 1);
+console.log(typeof Infinity);
+console.log(typeof NaN);
+console.log(2e-1);
+console.log(3e3);
+console.log(7.29e2);
+console.log(33e-1);
+console.log(7.1.toExponential());
+console.log(0.000009.toExponential());
+console.log(33.3.toExponential());
+console.log(10.001.toExponential());
+console.log(1.23456789.toExponential(5));
+console.log(1.23456789.toExponential(1));
+console.log(1.23456789.toExponential(4));
+console.log(1.23456789.toExponential(2));
+console.log(typeof(5).toString());
+console.log( typeof 77.12345.toString());
+console.log( typeof 0.1.toString());
+console.log( typeof 1e1.toString());
+console.log(12345.6789.toFixed());
+console.log(12345.6789.toFixed(1));
+console.log(12345.6789.toFixed(2));
+console.log(12345.6789.toFixed(6));
+console.log(1.23e+10.toFixed());
+console.log(1.23e+10.toFixed(1));
+console.log(1.23e+10.toFixed(2));
+console.log(1.23e+10.toFixed(6));
+console.log(1.23e-10.toFixed());
+console.log(1.23e-10.toFixed(1));
+console.log(1.23e-10.toFixed(2));
+console.log(1.23e-10.toFixed(6));
+console.log(12345.6789.toPrecision());
+console.log(12345.6789.toPrecision(1));
+console.log(12345.6789.toPrecision(2));
+console.log(12345.6789.toPrecision(6));
+console.log(1.23e+10.toPrecision());
+console.log(1.23e+10.toPrecision(1));
+console.log(1.23e+10.toPrecision(2));
+console.log(1.23e+10.toPrecision(6));
+console.log(1.23e-10.toPrecision());
+
+console.log(1.23e-10.toPrecision(1));
+console.log(1.23e-10.toPrecision(2));
+console.log(1.23e-10.toPrecision(6));
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+console.log(Number.NAN);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);*/
+/*console.log("привет\n Мир")
+var A="Emil", B="Mustafaev";
+console.log(A);
+console.log(A.length);
+console.log(B);
+console.log(B.length);
+console.log(A.length+B.length);
+var A='Шла cаша по шоссе'
+console.log("Первые две буквы"+A.charAt(0)+A.charAt(1));
+console.log("Последние две буквы-"+A.charAt(A.length-2)+A.charAt(A.length-1));
+console.log("Позиция первой пустной подстроки: " +A.indexOf("шоссе",5));
+var newStr=A.slice(0,4)+A[4].toUpperCase()+A.slice(5,A.length)
+console.log(newStr)
+console.log(A.toLowerCase("Саша"));
+var B='Не хочу учится а хочу женится';
+console.log(B.slice(0,B.indexOf("тся"))+"ться," + B.slice(B.indexOf("a"), B.indexOf("тся")) + "ться");*/
+var One= "One";
+var Two= "Two";
+var Three= "Three";
+var array= ["One","Two","Three"];
+var arry2 = new Array(3);
+console.log(arry2);
+var numbers=new Array(2, 3, 10, 5);
+console.log(numbers[1], numbers[numbers.length-2]);
+
+var letters =new Array("a", "b", "c", "f")
+console.log(letters)
+console.log(letters[2], letters[1], letters[0], letters[3])
+
+var massiv =new Array("1", "2", "3")
+console.log(massiv)
